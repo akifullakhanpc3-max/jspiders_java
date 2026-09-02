dd5680ff968933eb2d305e41e615bd8d2298b91a
asrar <akifullakhanpc3@gmail.com>
2026-09-02 18:37:04 +0530

    variable assignment

---
+++ Demo.java
@@ -0,0 +1,9 @@
+
+public class Demo{
+    public static void main(String[] args) {
+        int a = 10;int b=20;
+        System.out.println("Hello java");
+        System.out.print(a);
+        System.out.printf("the value of a is "+ a +"THE value of b is" + b);
+    }
+}
---
+++ assignments/Variables.java
@@ -0,0 +1,80 @@
+class Variable{
+    public static void main(String[] args) {
+
+        //1.program mobile
+        double mcost = 25000.0;
+        String mname = "IQOO neo 10R", mcolor = "White";
+        System.out.println("Mobile cost:"+ mcost);
+        System.out.println("Mobile name:"+ mname);
+        System.out.println("Mobile color:"+ mcolor);  
+        
+        System.out.println();
+        
+        //2.program car
+        double ccost = 1500000.0;
+        String cname = "BMW", ccolor = "White";
+        System.out.println("\ncar cost:"+ ccost);
+        System.out.println("car name:"+ cname);
+        System.out.println("car color:"+ ccolor);
+
+        //3.school
+        int strength = 60;
+        String sname = "IQOO neo 10R" ;
+        char grade = 'A';
+        System.out.println("\n\nSchool Strength:"+ strength);
+        System.out.println("School name:"+ sname);
+        System.out.println("School grade:"+ grade);
+
+        //4.program tv
+        double tv_cost = 2500.0;
+        String tv_brand= "IQOO neo 10R", tv_type = "White";
+        System.out.println("\n\nTV cost:"+ tv_cost);
+        System.out.println("TV brand:"+ tv_brand);
+        System.out.println("TV type: "+ tv_type); 
+
+        //5.program bike
+        double bike_cost = 250000.0;
+        String bike_name = "KTM", bike_color = "White";
+        System.out.println("\n\nbike cost:"+ bike_cost);
+        System.out.println("bike name:"+ bike_name);
+        System.out.println("bike color:"+ bike_color);
+
+        //6.program course
+        double exam_cost = 31000.0;
+        String course_name = "ISE", unvi_name= "VTU";
+        System.out.println("\n\ncourse cost:"+ exam_cost);
+        System.out.println("university name:"+ unvi_name);
+        System.out.println("course name:"+ course_name);
+
+        //7.program laptop
+        double lapi_cost = 55000.0;
+        String lapi_name = "predator helios neo 16", lapi_brand = "ACER";
+        System.out.println("\n\nLaptop cost:"+ lapi_cost);
+        System.out.println("Laptop name:"+ lapi_name);
+        System.out.println("Laptop brand:"+ lapi_brand);
+
+        //8.program home
+        double home_cost = 350000.0;
+        String home_name= "2BHK", home_color = "Green";
+        System.out.println("\n\nHome cost:"+ home_cost+" rupees");
+        System.out.println("Home name:"+ home_name);
+        System.out.println("Home color:"+ home_color);
+
+        //9.progrma emp
+        int emp_id=191;
+        double emp_sal = 3500242.65;
+        char emp_grade = 'B';
+        System.out.println("\n\nemployee id:"+emp_id);
+        System.out.println("employee salary"+emp_sal);
+        System.out.println("employee grade"+emp_grade);
+
+        //10.program cloth
+        String cloth_color = "blue";
+        char cloth_size = 'L';
+        double cloth_cost = 2500.62;
+        System.out.println("\n\ncloth color :"+ cloth_color );
+        System.out.println("cloth size :"+cloth_size );
+        System.out.println("Cloth cost :"+cloth_cost);
+
+    }
+}
